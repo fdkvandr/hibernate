ebb34c18f6bdacd499eb65babf9cdd01bb96948b
andrey.fedyakov <andrey.fedyakov@glowbyteconsulting.com>
2022-11-26 00:28:33 +0300

    Entity

--- src/main/java/com/corp/HibernateRunner.java
+++ src/main/java/com/corp/HibernateRunner.java
@@ -1,10 +1,13 @@
 package com.corp;
 
+import com.corp.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
 import org.hibernate.cfg.Configuration;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class HibernateRunner {
 
@@ -12,9 +15,23 @@ public class HibernateRunner {
 
         Configuration configuration = new Configuration();
         configuration.configure();
+        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
+        configuration.addAnnotatedClass(User.class);
         try (SessionFactory sessionFactory = configuration.buildSessionFactory()) {
             Session session = sessionFactory.openSession();
-            System.out.println("OK");
+
+            User user = User.builder()
+                    .username("ivan1@gmail.com")
+                    .firstname("Ivan")
+                    .lastname("Ivanov")
+                    .birthDate(LocalDate.of(2000, 1, 19))
+                    .age(20)
+                    .build();
+
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction()
+                    .commit();
         }
     }
 }
