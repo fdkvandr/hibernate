47d8c094d3a190f260ae4405eb5b83c6ef31eb06
Andrey Fedyakov <ya.andreyfedyakov@yandex.ru>
2022-11-28 09:43:28 +0300

    File appender log4j

--- src/main/java/com/corp/HibernateRunner.java
+++ src/main/java/com/corp/HibernateRunner.java
@@ -1,19 +1,17 @@
 package com.corp;
 
 import com.corp.entity.User;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.sql.SQLException;
 
 import static com.corp.util.HibernateUtil.buildSessionFactory;
 
+@Slf4j
 public class HibernateRunner {
-
-    public static final Logger log = LoggerFactory.getLogger(HibernateRunner.class);
 
     public static void main(String[] args) throws SQLException {
 
